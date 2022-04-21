1fd19607253f586f07c2213e28bec86eefd74f68
SAUL NJIE <SAULUS92@gmail.com>
2022-04-21 13:09:49 +0200

    Trying some strapi cms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import type { InferGetStaticPropsType } from 'next'
 import { ProductsResponse } from '../types/products-response'
+import Image from 'next/image'
+import mainImage from '../public/img/mainbanner.jpg'
+
 
 export default function Home({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
+    <div className='main-container'>
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <pre>{JSON.stringify(products, null, 2)}</pre>
+      <p>THIS IS A TEXT</p>
+      <Image 
+      src={mainImage}
+      height={400}
+      width={500}/>
+    </div>
     </div>
   )
 }
@@ -19,10 +29,10 @@ export async function getStaticProps() {
   }
 
   const products: ProductsResponse = await response.json()
-
   return {
     props: {
       products: products.data,
     },
   }
 }
+
