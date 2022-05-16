293be60a9e32fe31c9664a6e5a483caef42266ae
SAUL NJIE <SAULUS92@gmail.com>
2022-05-16 22:44:45 +0200

    idk

--- pages/livesearch.js
+++ pages/livesearch.js
@@ -1,20 +1,20 @@
-export default function liveSearch() {
-  let cards = document.querySelectorAll('.all-products-container')
-  let search_query = document.getElementById('searchbox').value
-  for (var i = 0; i < cards.length; i++) {
-    if (cards[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
-      cards[i].classList.remove('hidden')
-    } else {
-      cards[i].classList.add('hidden')
-    }
-  }
-}
+// export default function liveSearch() {
+//   let cards = document.querySelectorAll('.all-products-container')
+//   let search_query = document.getElementById('searchbox').value
+//   for (var i = 0; i < cards.length; i++) {
+//     if (cards[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
+//       cards[i].classList.remove('hidden')
+//     } else {
+//       cards[i].classList.add('hidden')
+//     }
+//   }
+// }
 
-let typingTimer
-let typeInterval = 500
-let searchInput = document.getElementById('searchbox')
+// let typingTimer
+// let typeInterval = 500
+// let searchInput = document.getElementById('searchbox')
 
-searchInput.addEventListener('keyup', () => {
-  clearTimeout(typingTimer)
-  typingTimer = setTimeout(liveSearch, typeInterval)
-})
+// searchInput.addEventListener('keyup', () => {
+//   clearTimeout(typingTimer)
+//   typingTimer = setTimeout(liveSearch, typeInterval)
+// })
