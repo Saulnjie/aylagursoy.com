3af900e2ec921da9cb0f2e42ff4353bbc6bce44f
SAUL NJIE <SAULUS92@gmail.com>
2022-05-30 00:24:40 +0200

    cleaning

--- types/product-response.ts
+++ types/product-response.ts
@@ -1,30 +1,4 @@
 
-// export interface ProductResponse {
-//   data: Data;
-//   meta: Meta;
-// }
-
-// export interface Data {
-//   id:         number;
-//   attributes: Attributes;
-// }
-
-// export interface Attributes {
-//   title:          string;
-//   description:    string;
-//   createdAt:      Date;
-//   updatedAt:      Date;
-//   publishedAt:    Date;
-//   specifications: string;
-//   purchased:      string;
-//   // coverImage:     HTMLImageElement; Stemmer dette for å få images?
-//   // additionalImages: HTMLImageElement;
-// }
-
-// export interface Meta {
-// }
-
-
 export interface ProductResponse {
   data: Data;
   meta: Meta;
